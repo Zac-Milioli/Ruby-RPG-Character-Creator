46cfdc2f04f4b9edef058d34a19873832a56ca4e
Zac-Milioli <zacmilioli@gmail.com>
2024-04-09 13:55:35 -0300

    códigos iniciais do play

--- play.rb
+++ play.rb
@@ -3,3 +3,15 @@ require "functions/rrc.rb"
 include RRBS
 include RRCFile
 
+running = true
+
+while running
+    puts "\n"*50
+    puts "- "*50
+    puts "\nBem vindo ao Ruby RPG Character Creator!\n\nEscolha uma opção\n\n[ENTER] Sair\n"
+    puts "- "*50
+    selection = gets.chomp()
+    if selection == ""
+        running = false
+    end
+end
