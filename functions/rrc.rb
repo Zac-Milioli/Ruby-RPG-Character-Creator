01a2fd4a72b3c95cafd6df5690ceada7709ebe75
Zac-Milioli <zacmilioli@gmail.com>
2024-04-10 14:54:56 -0300

    Adição de exportação, seleção e modificação de rrcs

--- functions/rrc.rb
+++ functions/rrc.rb
@@ -1,8 +1,30 @@
 module RRCFile
-    class ExpRRC
-        attr_accessor :filepath
-        def initialize(filepath=nil)
-            @filepath = filepath
+
+    class FormatRRC
+        attr_accessor :fileformat, :character
+        def initialize(character)
+            @character = character
+            @fileformat = "---/name/---\n#{@character.name}\n---/type/---\n#{@character.type}\n---/power/---\n#{@character.power}\n---/style/---\n#{@character.style}\n--/description/--\n#{@character.description}" 
         end
     end
+
+    class ExpRRC
+        attr_accessor :filepath, :character
+        def initialize(character, filepath="exports/")
+            @character = character
+            @filepath = filepath
+        end
+        def export_rrc
+            body = FormatRRC.new(@character).fileformat
+            File.open("#{@filepath}/#{@character.name}_#{@character.type}.rrc", "w") do |file|
+                file.write(body)
+                file.close()
+            end
+        end
+    end
+
+    class ImpRRC
+        attr_accessor :filepath
+    end
 end
+
--- play.rb
+++ play.rb
@@ -8,10 +8,11 @@ instance_rrc = RRCS::MyRRCS.new
 
 while running
     puts "\n"*150
+    puts "Bem vindo ao Ruby RPG Character Creator!"
     puts "- "*50
-    puts "\nBem vindo ao Ruby RPG Character Creator!\n\nEscolha uma opção\n\n[1] Criar um novo personagem\n[2] Listar personagens\n\n[ENTER] Sair\n"
+    puts "\n\nEscolha uma opção\n\n[1] Criar um novo personagem\n[2] Listar e selecionar personagens\n\n[ENTER] Sair\n\n"
     puts "- "*50
-    selection = gets.chomp()
+    selection = gets.chomp
     case selection
     when ""
         running = false
@@ -22,9 +23,9 @@ while running
             puts "\n"*150
             puts "\n\tPersonagem\nNome: #{new_rrc.name}\nTipo: #{new_rrc.type}\nPoder: #{new_rrc.power}\nEstilo: #{new_rrc.style}\n\nDescrição:\n#{new_rrc.description}\n\n"
             puts "- "*50
-            puts "\nCriação de personagem\n\nEscolha uma opção para modificar\n\n[1] Nome\n[2] Tipo\n[3] Poder\n[4] Estilo de luta\n[5] Descrição\n\n[0] Salvar e sair\n[ENTER] Cancelar e sair\n"
+            puts "\nCriação de personagem\n\nEscolha uma opção para modificar\n\n[1] Nome\n[2] Tipo\n[3] Poder\n[4] Estilo de luta\n[5] Descrição\n\n[0] Salvar e sair\n[ENTER] Cancelar e sair\n\n"
             puts "- "*50
-            opt = gets.chomp()
+            opt = gets.chomp
             case opt
             when "0"
                 instance_rrc.add_character(new_rrc)
@@ -33,36 +34,82 @@ while running
                 puts "\n"*150
                 puts "Digite o que quiser. Aperte ENTER para salvar."
                 puts "- "*50
-                new_rrc.name = gets.chomp()
+                new_rrc.name = gets.chomp.gsub(".", "").gsub("/", "").gsub(";", "").gsub(",", "").gsub(":", "").gsub("<", "").gsub(">", "").gsub("\\", "")
             when "2"
                 puts "\n"*150
                 puts "Digite o que quiser. Aperte ENTER para salvar."
                 puts "- "*50
-                new_rrc.type = gets.chomp()
+                new_rrc.type = gets.chomp.gsub(".", "").gsub("/", "").gsub(";", "").gsub(",", "").gsub(":", "").gsub("<", "").gsub(">", "").gsub("\\", "")
             when "3"
                 puts "\n"*150
                 puts "Digite o que quiser. Aperte ENTER para salvar."
                 puts "- "*50
-                new_rrc.power = gets.chomp()
+                new_rrc.power = gets.chomp
             when "4"
                 puts "\n"*150
                 puts "Digite o que quiser. Aperte ENTER para salvar."
                 puts "- "*50
-                new_rrc.style = gets.chomp()
+                new_rrc.style = gets.chomp
             when "5"
                 puts "\n"*150
                 puts "Digite o que quiser. Aperte ENTER para salvar."
                 puts "- "*50
-                new_rrc.description = gets.chomp()
+                new_rrc.description = gets.chomp
             else
                 creating = false
             end    
         end
     when "2"
         puts "\n"*150
+        puts "\tPersonagens salvos"
+        puts "- "*50
         puts instance_rrc.listing
-        puts "\n\nPressione ENTER para voltar ao menu"
-        gets
+        puts "- "*50
+        puts "\n\nPressione ENTER para voltar ao menu, ou digite\nexatamente o NOME de algum personagem para selecioná-lo\n"
+        character_selected = instance_rrc.find_character(gets.chomp)
+        if character_selected != nil && character_selected != ""
+            creating = true
+            while creating
+                puts "\n"*150
+                puts "\n\tPersonagem\nNome: #{character_selected.name}\nTipo: #{character_selected.type}\nPoder: #{character_selected.power}\nEstilo: #{character_selected.style}\n\nDescrição:\n#{character_selected.description}\n\n"
+                puts "- "*50
+                puts "\nAções de personagem\n\nEscolha uma opção para modificar ou executar\n\n[1] Nome\n[2] Tipo\n[3] Poder\n[4] Estilo de luta\n[5] Descrição\n\n[0] Exportar personagem\n[ENTER] Sair\n\n"
+                puts "- "*50
+                opt = gets.chomp
+                case opt
+                when "0"
+                    RRCFile::ExpRRC.new(character=character_selected).export_rrc
+                    creating = false
+                when "1"
+                    puts "\n"*150
+                    puts "Digite o que quiser. Aperte ENTER para salvar."
+                    puts "- "*50
+                    character_selected.name = gets.chomp.gsub(".", "").gsub("/", "").gsub(";", "").gsub(",", "").gsub(":", "").gsub("<", "").gsub(">", "").gsub("\\", "")
+                when "2"
+                    puts "\n"*150
+                    puts "Digite o que quiser. Aperte ENTER para salvar."
+                    puts "- "*50
+                    character_selected.type = gets.chomp.gsub(".", "").gsub("/", "").gsub(";", "").gsub(",", "").gsub(":", "").gsub("<", "").gsub(">", "").gsub("\\", "")
+                when "3"
+                    puts "\n"*150
+                    puts "Digite o que quiser. Aperte ENTER para salvar."
+                    puts "- "*50
+                    character_selected.power = gets.chomp
+                when "4"
+                    puts "\n"*150
+                    puts "Digite o que quiser. Aperte ENTER para salvar."
+                    puts "- "*50
+                    character_selected.style = gets.chomp
+                when "5"
+                    puts "\n"*150
+                    puts "Digite o que quiser. Aperte ENTER para salvar."
+                    puts "- "*50
+                    character_selected.description = gets.chomp
+                else
+                    creating = false
+                end    
+            end  
+        end
     else
         puts "\n\nEscolha uma opção válida!"
         puts "Pressione ENTER para voltar ao menu"
